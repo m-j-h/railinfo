4288b1ab8bc903058694c048f78b4fb164c04b36
Mark <mh2004abc@yahoo.co.uk>
2017-06-17 18:10:19 +0100

    Start to add json parser tests

---
+++ src/json/jsonparsertests.cpp
@@ -0,0 +1,11 @@
+
+#define BOOST_TEST_MODULE JSON Parser Tests
+#include <boost/test/included/unit_test.hpp>
+
+#include "jsonparser.h"
+
+BOOST_AUTO_TEST_CASE( firstTest )
+{
+    BOOST_REQUIRE(true);
+}
+
