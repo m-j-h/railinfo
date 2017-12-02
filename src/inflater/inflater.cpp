cbb0a7695ece209b3ec0b68fd3418149608d677a
mark <me@me>
2017-12-02 23:21:55 +0000

    Got inflater working

--- src/inflater/inflater.cpp
+++ src/inflater/inflater.cpp
@@ -1,5 +1,11 @@
 #include "inflater.h"
 
+#include <stdexcept>
+#include <zlib.h>
+
+#include <algorithm>
+#include <iostream>
+
 Inflater::Inflater()
 {}
 
@@ -8,6 +14,46 @@ Inflater::~Inflater()
     
 std::string Inflater::Process( const std::string& source )
 {
-    return source;  // TODO: Implement this
+    const size_t CHUNK = 4096;
+    unsigned char in[CHUNK];
+    unsigned char out[CHUNK];
+    std::string result;
+
+    z_stream zs;
+    zs.zalloc   = Z_NULL;
+    zs.zfree    = Z_NULL;
+    zs.opaque   = Z_NULL;
+    zs.avail_in = 0;
+    zs.next_in  = Z_NULL;
+
+    int rc = inflateInit2(&zs, 15 | 32 );
+    if( rc != Z_OK )
+    {
+        throw std::runtime_error("zlib.inflateInit2() failed");
+    }
+    zs.avail_in = source.length();
+    zs.next_in  = (unsigned char*)(source.data());
+
+    do
+    {
+        zs.avail_out = CHUNK;
+        zs.next_out  = out;
+        rc = inflate(&zs, Z_NO_FLUSH);
+        switch( rc )
+        {
+            case Z_NEED_DICT:
+            case Z_DATA_ERROR:
+            case Z_MEM_ERROR:
+                inflateEnd(&zs);
+                const std::string msg = "zlib.inflate() failed";
+                throw std::runtime_error(msg);
+                break;
+        }
+        unsigned have = CHUNK - zs.avail_out;
+        result += std::string( (const char*)out, have );
+    } while( zs.avail_out == 0 );
+
+    inflateEnd(&zs);
+    return result;
 }
 
--- src/inflater/inflatertests.cpp
+++ src/inflater/inflatertests.cpp
@@ -3,13 +3,51 @@
 #include <boost/test/included/unit_test.hpp>
 
 #include <string>
+#include <fstream>
+#include <algorithm>
+#include <iostream>
+
 #include "inflater.h"
 
+// Helpers
+void AssertFileOpen( std::ifstream& is, const std::string& filename )
+{
+    if( !is.is_open() )
+    {
+        throw std::runtime_error("Failed to open file '" + filename + "'");
+    }
+}
+
+std::string ReadBinaryFile(const std::string& filename)
+{
+    std::ifstream fs(filename.c_str(), std::ios::binary );
+    AssertFileOpen( fs, filename );
+
+    std::istreambuf_iterator<char> begin( fs );
+    std::istreambuf_iterator<char> end;
+    std::string content;
+    std::copy( begin, end, std::back_inserter( content ) );
+    return content;
+}
+
+std::string ReadTextLine(const std::string& filename)
+{
+    std::ifstream fs(filename.c_str());
+    AssertFileOpen( fs, filename );
+
+    std::string line;
+    std::getline( fs, line );
+    return line;
+}
+
+// Tests
 BOOST_AUTO_TEST_CASE( CompleteMessageWithEmbeddedNulWithExtraDataAtEnd )
 {
+    std::string compressed = ReadBinaryFile( "./data/data.gz" );
+    std::string expected   = ReadTextLine(   "./data/data.xml" );
+
     Inflater target;
-    const std::string source = "Some invalid data";
-    const std::string result = target.Process(source);
-    BOOST_REQUIRE( result == source );
+    const std::string result = target.Process(compressed);
+    BOOST_REQUIRE( result == expected );
 }
 
